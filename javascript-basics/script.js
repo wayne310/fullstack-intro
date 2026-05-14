6fef14041ec662c97a12d0f9b207c6a44db6f00c
John Wayne C. Arguelles <nicehehehe078@gmail.com>
2026-05-14 10:46:47 +0800

    done

---
+++ javascript-basics/script.js
@@ -0,0 +1,53 @@
+// Step 2: Variables
+
+var studentName = "Wayne";
+let age = 18;
+const school = "BSIT";
+
+// Display Variable Values
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Course:", school);
+
+
+// Step 3: Data Types
+
+let numberExample = 100;
+let stringExample = "Hello World";
+let booleanExample = true;
+let arrayExample = ["Apple", "Banana", "Orange"];
+let objectExample = {
+    brand: "Redmi",
+    model: "Note 10"
+};
+
+// Display Data Types
+console.log(typeof numberExample);
+console.log(typeof stringExample);
+console.log(typeof booleanExample);
+console.log(typeof arrayExample);
+console.log(typeof objectExample);
+
+
+// Step 4: Operators
+
+// Arithmetic Operators
+let a = 20;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+
+// Comparison and Logical Operators
+let x = 10;
+let y = 15;
+
+console.log(x > y);
+console.log(x < y);
+console.log(x == y);
+
+console.log(x < y && y > x);
+console.log(x > y || y > x);
