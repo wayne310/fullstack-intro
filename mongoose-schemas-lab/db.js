85524f57bd8b6f051efb970fb94e4676f3009ce0
John Wayne C. Arguelles <nicehehehe078@gmail.com>
2026-05-15 12:04:04 +0800

    Completed mongoose schemas lab

--- mongoose-schemas-lab/db.js
+++ mongoose-schemas-lab/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/mongoose_lab');
+
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
